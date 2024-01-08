8c1bb7ff547db8a48f9f3d9f6e8660b16ae3f3ab
unknown <pallavipatelsanugula@gmail.com>
2024-01-08 21:25:42 +0530

    react first task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
+//3/11/2023 task
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <ol>
+    <li>apple</li>
+      <li>banana</li>
+     </ol>
+     <ul>
+      <li>one</li>
+      <li>twooooo</li>
+     </ul>
+    <table>
+      <tr>
+        <th>name</th>
+        <th>age</th>
+        <th>interests</th>
+      </tr>
+      <tr>
+        <td>pal</td>
+        <td>21</td>
+        <td>novels</td>
+      </tr>
+      <tr>
+        <td>san</td>
+        <td>22</td>
+        <td>movies</td>
+      </tr>
+    </table>
+    <form>
+      <input type="text" value="hello"></input><br></br>
+      <button>submit</button><br></br><br></br>
+      <label for="name">name:</label>
+      <input type="text" value="pallavi" id="name"></input>
+    </form>
+
     </div>
+    
   );
 }
 
