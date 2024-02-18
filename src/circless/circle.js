fee203ab30b6bde67bea5b7f29638cc134dbfa03
pallavipatelsanugula2002 <pallavipatelsanugula@gmail.com>
2024-02-18 15:26:47 +0530

    done

---
+++ src/circless/circle.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const Circle = () => {
+  const [circles, setCircles] = useState([]);
+
+  const addCircle = () => {
+    setCircles([...circles, false]);
+  };
+
+  const circleClick = (index) => {
+    const updateCircles = circles.map((value, id) =>
+      id=== index ? !value : value
+    );
+    setCircles(updateCircles);
+  };
+
+  const clickedCirclesCount=circles.filter(value=>value).length
+
+
+  return (
+    <>
+      <button onClick={addCircle}>Click to add Circle</button>
+      <h2> Total Circle :  {circles.length} </h2>
+      <h2>Circles clicked : {clickedCirclesCount}</h2>
+      <h2>Circles not clicked : {circles.length-clickedCirclesCount}</h2>
+      <div style={{display: "flex", flexWrap: "wrap", gap: "10px"}}>
+      {circles.map((value, index) => (
+        <div
+          key={index}
+          style={{
+            height: "80px",
+            width: "80px",
+            border: "4px solid black",
+            borderRadius: "50%",
+            backgroundColor: value ? "red" : "black",
+          }}
+          onClick={() => circleClick(index)}
+        ></div>
+      ))}
+      </div>
+    </>
+  );
+};
+
+
+export default Circle;
